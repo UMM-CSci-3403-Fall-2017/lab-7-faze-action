e27ca4b4bbf6f0111fe307b07c03228f10797aa9
cookx876 <cookx876@morris.umn.edu>
2017-10-31 11:32:10 -0500

    Make EchoServer multi-threaded in a simple way. Creates new thread for every client

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -10,24 +10,47 @@ public class EchoServer {
             ServerSocket sock = new ServerSocket(6013);
 
             while(true){
-                System.out.println("Got a request!");
+                System.out.println("Ready for next request!");
                 Socket client = sock.accept();
 
-                OutputStream out = client.getOutputStream();
-                InputStream reader = client.getInputStream();
-
-                int nextByte;
-                // read from client, write to client
-                while((nextByte = reader.read()) != -1){
-                    out.write(nextByte);
-                }
-
-                out.flush();
-                client.shutdownOutput();
-                client.close();
+                // Start the new thread for this client
+				// It's important that we accept and then pass the client socket before creating the thread so there
+				// aren't infinite threads being created.
+                EchoThread echoThread = new EchoThread(client);
+                echoThread.start();
             }
         } catch (IOException ioe){
             System.err.println(ioe);
         }
     }
+
+    public static class EchoThread extends Thread {
+    	// Create the thread with a socket for the client connection
+    	Socket client;
+    	public EchoThread(Socket client){
+    		this.client = client;
+		}
+
+		// Read and write from/to client within thread
+		public void run(){
+    		// Don't throw IOException because you don't want the entire server to crash if this thread crashes
+    		try {
+				OutputStream out = client.getOutputStream();
+				InputStream reader = client.getInputStream();
+
+				int nextByte;
+				// read from client, write to client
+				while((nextByte = reader.read()) != -1){
+					out.write(nextByte);
+				}
+
+				// Close the socket gracefully
+				out.flush();
+				client.shutdownOutput();
+				client.close();
+    		} catch (IOException e){
+    			System.err.println(e);
+			}
+		}
+	}
 }
